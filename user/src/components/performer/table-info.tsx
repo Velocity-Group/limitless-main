acc3181cf731670a1b608095e2c3e6024190a435
Hung Hoai <tnhh24593@gmail.com>
2021-07-22 16:40:28 +0700

    update style

--- user/src/components/performer/table-info.tsx
+++ user/src/components/performer/table-info.tsx
@@ -12,7 +12,7 @@ interface IProps {
 export class PerformerInfo extends PureComponent<IProps> {
   render() {
     const { performer, countries = [] } = this.props;
-    const country = countries.length && countries.find((c) => c.code === performer.country);
+    const country = countries.length && countries.find((c) => c.code === performer?.country);
     return (
       <>
         <Descriptions className="performer-info">
@@ -23,32 +23,23 @@ export class PerformerInfo extends PureComponent<IProps> {
             {country?.name}
           </Descriptions.Item>
           )}
-          {performer.gender && (
+          {performer?.gender && (
           <Descriptions.Item label="Gender">
-            <Tag color="magenta">
-              <img
-                height="15px"
-                alt="performer-gender"
-                // eslint-disable-next-line no-nested-ternary
-                src={performer.gender === 'male' ? '/static/male.png' : performer.gender === 'female' ? '/static/female.png' : performer.gender === 'couple' ? '/static/couple.png' : '/static/transgender.png'}
-              />
-              {' '}
-              {performer.gender}
-            </Tag>
+            {performer?.gender}
           </Descriptions.Item>
           )}
-          {performer.dateOfBirth && <Descriptions.Item label="Date of Birth">{formatDateNoTime(performer.dateOfBirth)}</Descriptions.Item>}
-          {performer.bodyType && <Descriptions.Item label="Body Type">{performer.bodyType}</Descriptions.Item>}
-          {performer.state && <Descriptions.Item label="State/County/Province">{performer.state}</Descriptions.Item>}
-          {performer.city && <Descriptions.Item label="City">{performer.city}</Descriptions.Item>}
-          {performer.height && <Descriptions.Item label="Height">{performer.height}</Descriptions.Item>}
-          {performer.weight && <Descriptions.Item label="Weight">{performer.weight}</Descriptions.Item>}
-          {performer.eyes && <Descriptions.Item label="Eyes color">{performer.eyes}</Descriptions.Item>}
-          {performer.ethnicity && <Descriptions.Item label="Ethnicity">{performer.ethnicity}</Descriptions.Item>}
-          {performer.hair && <Descriptions.Item label="Hair color">{performer.hair}</Descriptions.Item>}
-          {performer.pubicHair && <Descriptions.Item label="Pubic Hair">{performer.pubicHair}</Descriptions.Item>}
-          {performer.bust && <Descriptions.Item label="Bust size">{performer.bust}</Descriptions.Item>}
-          {performer.sexualPreference && <Descriptions.Item label="Sexual reference">{performer.sexualPreference}</Descriptions.Item>}
+          {performer?.dateOfBirth && <Descriptions.Item label="Date of Birth">{formatDateNoTime(performer?.dateOfBirth)}</Descriptions.Item>}
+          {performer?.bodyType && <Descriptions.Item label="Body Type">{performer?.bodyType}</Descriptions.Item>}
+          {performer?.state && <Descriptions.Item label="State/County/Province">{performer?.state}</Descriptions.Item>}
+          {performer?.city && <Descriptions.Item label="City">{performer?.city}</Descriptions.Item>}
+          {performer?.height && <Descriptions.Item label="Height">{performer?.height}</Descriptions.Item>}
+          {performer?.weight && <Descriptions.Item label="Weight">{performer?.weight}</Descriptions.Item>}
+          {performer?.eyes && <Descriptions.Item label="Eyes color">{performer?.eyes}</Descriptions.Item>}
+          {performer?.ethnicity && <Descriptions.Item label="Ethnicity">{performer?.ethnicity}</Descriptions.Item>}
+          {performer?.hair && <Descriptions.Item label="Hair color">{performer?.hair}</Descriptions.Item>}
+          {performer?.pubicHair && <Descriptions.Item label="Pubic Hair">{performer?.pubicHair}</Descriptions.Item>}
+          {performer?.bust && <Descriptions.Item label="Bust size">{performer?.bust}</Descriptions.Item>}
+          {performer?.sexualPreference && <Descriptions.Item label="Sexual reference">{performer?.sexualPreference}</Descriptions.Item>}
         </Descriptions>
       </>
     );
--- user/src/components/post/post-slider.tsx
+++ user/src/components/post/post-slider.tsx
@@ -1,7 +1,5 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
 import { PureComponent } from 'react';
-import { Modal, Carousel, Spin } from 'antd';
+import { Carousel, Spin, Image } from 'antd';
 import { VideoPlayer } from '@components/common/video-player';
 import './index.less';
 
@@ -10,27 +8,8 @@ interface IProps {
 }
 
 export default class FeedSlider extends PureComponent<IProps> {
-  state = {
-    visible: false,
-    imgUrl: ''
-  }
-
-  handleOk() {
-    this.setState({
-      visible: false
-    });
-  }
-
-  openModal(img) {
-    this.setState({
-      visible: true,
-      imgUrl: img.url
-    });
-  }
-
   render() {
     const { feed } = this.props;
-    const { visible, imgUrl } = this.state;
     const images = feed.files && feed.files.filter((f) => f.type === 'feed-photo');
     const videos = feed.files && feed.files.filter((f) => f.type === 'feed-video');
     let processing = false;
@@ -45,9 +24,7 @@ export default class FeedSlider extends PureComponent<IProps> {
         {!processing && feed.files && feed.files.length && (
         <Carousel swipeToSlide arrows dots={false}>
           {images && images.length > 0 && images.map((img) => (
-            <a title="Click to open" key={img._id} onClick={this.openModal.bind(this, img)}>
-              <img src={img.url} title={img.name} width="100%" alt="img" />
-            </a>
+            <Image src={img.url} title={img.name} width="100%" alt="img" />
           ))}
           {videos && videos.length > 0 && videos.map((vid) => (
             <VideoPlayer
@@ -78,16 +55,6 @@ export default class FeedSlider extends PureComponent<IProps> {
           {videos.length > 0 && images.length > 0 && '|'}
           {videos.length > 0 && <span>{videos.length} <VideoCameraOutlined /></span>}
         </div>} */}
-        <Modal
-          visible={visible}
-          onOk={this.handleOk.bind(this)}
-          onCancel={this.handleOk.bind(this)}
-          width={1200}
-        >
-          <div className="text-center">
-            <img alt="img" src={imgUrl} style={{ maxWidth: '100%' }} />
-          </div>
-        </Modal>
       </div>
     );
   }
