1c5d4903b7ecee05d28f8f51c22dfbd61dc1effc
Hung Hoai <tnhh24593@gmail.com>
2021-10-12 18:05:46 +0700

    chore

--- api/src/modules/comment/services/comment.service.ts
+++ api/src/modules/comment/services/comment.service.ts
@@ -52,9 +52,13 @@ export class CommentService {
         data: new CommentDto(newComment)
       })
     );
+    const [performerInfo, userInfo] = await Promise.all([
+      this.performerService.findById(user._id),
+      this.userService.findById(user._id)
+    ]);
     const returnData = new CommentDto(newComment);
     // eslint-disable-next-line no-nested-ternary
-    returnData.creator = new UserDto(user).toResponse();
+    returnData.creator = (userInfo && new UserDto(userInfo).toResponse()) || (performerInfo && new PerformerDto(performerInfo).toResponse());
     return returnData;
   }
 
