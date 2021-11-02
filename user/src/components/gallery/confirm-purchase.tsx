da765911a1aa5b55a958f3afd2eb20367cbb59c1
Hieu Nguyen <trunghieu7710@gmail.com>
2021-11-02 12:25:29 +0700

    gallery card - update style

--- user/src/components/gallery/confirm-purchase.tsx
+++ user/src/components/gallery/confirm-purchase.tsx
@@ -1,8 +1,6 @@
-import { PureComponent } from 'react';
-import {
-  Button
-} from 'antd';
-import { IGallery } from '@interfaces/index';
+import { PureComponent } from "react";
+import { Button } from "antd";
+import { IGallery } from "@interfaces/index";
 
 interface IProps {
   submiting: boolean;
@@ -13,15 +11,22 @@ interface IProps {
 export class PurchaseGalleryForm extends PureComponent<IProps> {
   render() {
     const { gallery, onFinish, submiting } = this.props;
-    const image = gallery?.coverPhoto?.thumbnails[0] || '/static/no-image.jpg';
+    const image = gallery?.coverPhoto?.thumbnails[0] || "/static/no-image.jpg";
 
     return (
       <div className="text-center">
         <div className="tip-performer">
-          <img alt="p-avt" src={image} style={{ width: '220px', borderRadius: '5px' }} />
-          <h4>
-            {gallery?.title}
-          </h4>
+          <img
+            alt="p-avt"
+            src={image}
+            style={{
+              width: "220px",
+              borderRadius: "5px",
+              filter: "blur(20px)",
+              marginBottom: 10
+            }}
+          />
+          <h4>{gallery?.title}</h4>
           <p>{gallery?.description}</p>
         </div>
         <div className="text-center">
@@ -33,11 +38,15 @@ export class PurchaseGalleryForm extends PureComponent<IProps> {
             disabled={submiting}
           >
             Confirm to unlock by &nbsp;
-            <img alt="token" src="/static/coin-ico.png" height="15px" style={{ margin: '0 3px' }} />
+            <img
+              alt="token"
+              src="/static/coin-ico.png"
+              height="15px"
+              style={{ margin: "0 3px" }}
+            />
             {(gallery?.price).toFixed(2)}
           </Button>
         </div>
-
       </div>
     );
   }
