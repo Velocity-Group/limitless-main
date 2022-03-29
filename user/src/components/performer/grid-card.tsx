94ed59b2dbffc58533a0b98e44bcea5538f20cd5
Thiên Nguyễn <broagii@yahoo.com>
2022-03-30 06:58:35 +0700

    redirect to streaming page when click live label

--- user/src/components/performer/grid-card.tsx
+++ user/src/components/performer/grid-card.tsx
@@ -1,16 +1,41 @@
 import { PureComponent } from 'react';
-import { IPerformer } from 'src/interfaces';
+import { IPerformer, IUser } from 'src/interfaces';
 import Link from 'next/link';
 import { StarOutlined } from '@ant-design/icons';
 import { TickIcon } from 'src/icons';
 import { dobToAge, shortenLargeNumber } from '@lib/index';
 import './performer.less';
+import { connect } from 'react-redux';
+import { message } from 'antd';
+import Router from 'next/router';
 
 interface IProps {
   performer: IPerformer;
+  currentUser: IUser;
 }
 
-export default class PerformerGridCard extends PureComponent<IProps> {
+class PerformerGridCard extends PureComponent<IProps> {
+  handleJoinStream = (e) => {
+    e.preventDefault();
+    const { currentUser, performer } = this.props;
+    if (!currentUser._id) {
+      message.error('Please log in or register!');
+      return;
+    }
+    if (currentUser.isPerformer) return;
+    if (!performer?.isSubscribed) {
+      message.error('Please subscribe to this model!');
+      return;
+    }
+    Router.push({
+      pathname: '/streaming/details',
+      query: {
+        performer: JSON.stringify(performer),
+        username: performer?.username || performer?._id
+      }
+    }, `/streaming/${performer?.username || performer?._id}`);
+  }
+
   render() {
     const { performer } = this.props;
     return (
@@ -48,3 +73,6 @@ export default class PerformerGridCard extends PureComponent<IProps> {
     );
   }
 }
+
+const maptStateToProps = (state) => ({ currentUser: { ...state.user.current } });
+export default connect(maptStateToProps)(PerformerGridCard);
