51db50fd2f415931580064749e46f7b4f7db3bb8
Hung Hoai <tnhh24593@gmail.com>
2022-04-06 12:03:18 +0700

    message - fix count total not read

--- api/src/modules/message/services/notification-message.service.ts
+++ api/src/modules/message/services/notification-message.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, Inject } from '@nestjs/common';
+import { ObjectId } from 'mongodb';
 import { Model } from 'mongoose';
 import { SocketUserService } from 'src/modules/socket/services/socket-user.service';
 import { UserDto } from 'src/modules/user/dtos';
@@ -18,12 +19,12 @@ export class NotificationMessageService {
       recipientId: user._id,
       conversationId
     }, { totalNotReadMessage: 0 });
-    const total = await this.countTotalNotReadMessage(user._id.toString());
+    const total = await this.countTotalNotReadMessage(user._id);
     await this.socketUserService.emitToUsers(user._id, 'nofify_read_messages_in_conversation', total);
     return { ok: true };
   }
 
-  public async countTotalNotReadMessage(userId: string): Promise<any> {
+  public async countTotalNotReadMessage(userId: ObjectId): Promise<any> {
     const totalNotReadMessage = await this.notificationMessageModel.aggregate<any>([
       {
         $match: { recipientId: userId }
