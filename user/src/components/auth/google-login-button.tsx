a8f96389e729cfb7f4c65c90302033fc233c0e2b
Minh Quan <tranminhquann120296>
2022-05-30 15:07:33 +0700

    show the google login button if no prompt appears
    (cherry picked from commit ac29b631bbf6cf8070909b4056a3489b1ac3f256)

--- user/src/components/auth/google-login-button.tsx
+++ user/src/components/auth/google-login-button.tsx
@@ -1,24 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { GoogleOutlined } from '@ant-design/icons';
 import useGoogleLogin from '@lib/hook/use-google-login';
-
+import { Typography } from 'antd';
+const { Text } = Typography;
+import './google-login-button.less';
 interface IProps {
   clientId: string;
   onSuccess: Function;
   onFailure: Function;
 }
 const GoogleLoginButton = ({ clientId, onSuccess, onFailure }: IProps) => {
-  const { signIn, loaded } = useGoogleLogin({
+  const { signIn, loaded, renderButtonSignIn } = useGoogleLogin({
     clientId,
     onSuccess,
     onFailure,
     onScriptLoadFailure: onFailure
   });
 
+  const [clickedOnGoogleLoginButton, setClicked] = useState(false);
+
+  const loginWithGoogle = () => {
+    setClicked(true);
+    signIn();
+  };
+
+  useEffect(() => {
+    if (clickedOnGoogleLoginButton) {
+      renderButtonSignIn();
+    }
+  }, [clickedOnGoogleLoginButton]);
+
   return (
-    <button type="button" disabled={!clientId || !loaded} onClick={() => signIn()} className="google-button">
-      <GoogleOutlined /> LOG IN / SIGN UP WITH GOOGLE
-    </button>
+    <>
+      <button type="button" disabled={!clientId || !loaded} onClick={() => loginWithGoogle()} className="google-button">
+        <GoogleOutlined /> LOG IN / SIGN UP WITH GOOGLE
+      </button>
+      {clickedOnGoogleLoginButton && (
+        <div className="btn-google-login-box">
+          <Text type="secondary">
+            If no prompt appears just click the button bellow to start the authentication flow:
+          </Text>
+          <div id="btnLoginWithGoogle" className="btn-google-login"></div>
+        </div>
+      )}
+    </>
   );
 };
 
